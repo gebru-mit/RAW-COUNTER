3893d9f00f7bee5bf84cad2adb4bf42dc91b2c78
gebru-mit <gebru2008@gmail.com>
2023-11-21 13:43:16 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count=0
+function increment(){
+    count += 1 
+    countEl.innerText = count
+}
+
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+}  
